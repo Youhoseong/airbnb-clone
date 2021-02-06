52e47c53c426773ad9a8a4083d8103775eefe0dc
Youhoseong <h0song@naver.com>
2021-02-06 23:33:48 +0900

    update:

--- core/models.py
+++ core/models.py
@@ -6,8 +6,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamp Model Definition """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
 
     class Meta:
         abstract = True
--- rooms/templatetags/sexy_capitals.py
+++ rooms/templatetags/sexy_capitals.py
@@ -1,7 +1,5 @@
 from django import template
 
-
-
 # example making filter
 
 register = template.Library()
